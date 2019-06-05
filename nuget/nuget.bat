ff01e7fd9ca93910e2f427fe85bc1b85787963d1
Dale Newman <dalenewman@gmail.com>
2019-06-04 22:35:27 -0400

    added autofac v3 version to support orchard 1.10.2

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,5 +1,7 @@
 nuget pack Transformalize.Provider.Excel.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.Excel.Autofac.nuspec -OutputDirectory "c:\temp\modules"
+nuget pack Transformalize.Provider.Excel.Autofac.v3.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.Excel.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Excel.Autofac.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Excel.0.6.6-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Excel.Autofac.0.6.6-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Excel.Autofac.v3.0.6.6-beta.nupkg" -source https://api.nuget.org/v3/index.json
--- src/Transformalize.Provider.Excel.Autofac.v3/Properties/AssemblyInfo.cs
+++ src/Transformalize.Provider.Excel.Autofac.v3/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Transformalize.Provider.Excel.Autofac")]
-[assembly: AssemblyDescription("An Autofac Container for Transformalize Excel")]
+[assembly: AssemblyTitle("Transformalize.Provider.Excel.Autofac.v3")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Transformalize")]
-[assembly: AssemblyProduct("Transformalize.Provider.Excel.Autofac")]
-[assembly: AssemblyCopyright("Copyright © 2013-2019 Dale Newman")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Transformalize.Provider.Excel.Autofac.v3")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("61f77bc9-21e5-454e-876c-6d93daf641e7")]
+[assembly: Guid("8846d844-b947-4159-88b2-daca180a4a52")]
 
 // Version information for an assembly consists of the following four values:
 //
