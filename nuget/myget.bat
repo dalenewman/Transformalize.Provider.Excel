3c8b3832dba1ba5a9ea45b7724ca3dd1f3e98ed7
Dale Newman <dalenewman@gmail.com>
2020-11-22 20:24:24 -0500

    added .net standard target (for reader)

--- nuget/myget.bat
+++ nuget/myget.bat
@@ -1,7 +1,7 @@
-nuget pack Transformalize.Provider.Excel.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Provider.Excel.Autofac.nuspec -OutputDirectory "c:\temp\modules"
-nuget pack Transformalize.Provider.Excel.Autofac.v3.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Provider.Excel.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Provider.Excel.Autofac.nuspec -OutputDirectory "c:\temp\modules"
+REM nuget pack Transformalize.Provider.Excel.Autofac.v3.nuspec -OutputDirectory "c:\temp\modules"
 
-nuget push "c:\temp\modules\Transformalize.Provider.Excel.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.Excel.Autofac.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.Excel.Autofac.v3.0.8.22-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Excel.0.8.29-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Excel.Autofac.0.8.29-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Excel.Autofac.v3.0.8.29-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -2,6 +2,6 @@ nuget pack Transformalize.Provider.Excel.nuspec -OutputDirectory "c:\temp\module
 nuget pack Transformalize.Provider.Excel.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.Excel.Autofac.v3.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.Excel.0.6.21-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Excel.Autofac.0.6.21-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Excel.Autofac.v3.0.6.21-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Excel.0.6.29-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Excel.Autofac.0.6.29-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Excel.Autofac.v3.0.6.29-beta.nupkg" -source https://api.nuget.org/v3/index.json
